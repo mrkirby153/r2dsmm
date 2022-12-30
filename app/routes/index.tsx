a679455830d56479d4c99ac1efc961e971f8f2fc
mrkirby153 <mr.austinwhyte@gmail.com>
2022-12-30 15:08:47 -0800

    Redesign index and serverlist

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,35 +3,35 @@ import { Button, ButtonStyle } from "~/components/Button";
 import { LogoutForm } from "~/components/FormInput";
 import { useOptionalUser } from "~/utils";
 
-export default function Index() {
+function LoginButtons() {
   const user = useOptionalUser();
+
+  if (user) {
+    return (
+      <LogoutForm>
+        <Button type="submit" buttonStyle={ButtonStyle.Danger}>
+          Log Out
+        </Button>
+      </LogoutForm>
+    );
+  } else {
+    return (
+      <>
+        <Link to={"/register"} className="ml-3 rounded bg-blue-500 py-2 px-4 text-center text-white hover:bg-blue-600">Register</Link>
+        <Link to={"/login"} className="ml-3 rounded bg-green-500 py-2 px-4 text-center text-white hover:bg-green-600">Login</Link>
+      </>
+    );
+  }
+}
+
+export default function Index() {
   return (
     <div className="flex min-h-full flex-col justify-center">
-      <div className="mx-auto w-full max-w-md px-8">
-        Welcome to the Risk of Rain Dedicated Server Mod Manager
+      <div className="mx-auto w-full max-w-[50%] text-center">
+        <h1 className="pb-3 text-5xl">R2DSMM</h1>
+        <p>Welcome to the Risk of Rain 2 Dedicated Server Mod Manager</p>
         <div className="mt-4">
-          {(!user && (
-            <>
-              <Link
-                to={"/register"}
-                className="rounded bg-blue-500 py-2 px-4 text-center text-white hover:bg-blue-600"
-              >
-                Register
-              </Link>
-              <Link
-                to={"/login"}
-                className="ml-3 rounded bg-green-500 py-2 px-4 text-center text-white hover:bg-green-600"
-              >
-                Login
-              </Link>
-            </>
-          )) || (
-            <LogoutForm>
-              <Button type="submit" buttonStyle={ButtonStyle.Danger}>
-                Logout
-              </Button>
-            </LogoutForm>
-          )}
+          <LoginButtons />
         </div>
       </div>
     </div>
--- app/routes/servers.tsx
+++ app/routes/servers.tsx
@@ -2,53 +2,114 @@ import { Link, useLoaderData } from "@remix-run/react";
 import type { LoaderArgs } from "@remix-run/server-runtime";
 import { getServersForUser } from "~/models/servers.server";
 import { requireUser } from "~/session.server";
-import  { GameMode } from "@prisma/client";
+import { GameMode } from "@prisma/client";
 import invariant from "tiny-invariant";
+import { createRef, useState } from "react";
+import { createPopper } from "@popperjs/core";
 
-export const loader = async({request}: LoaderArgs) => {
-    const user = await requireUser(request)
-    return getServersForUser(user)
-}
+import { FaTrash, FaEllipsisV } from "react-icons/fa";
+
+export const loader = async ({ request }: LoaderArgs) => {
+  const user = await requireUser(request);
+  return getServersForUser(user);
+};
 
 const GAME_MODE_TRANSLATIONS = {
-    [GameMode.CLASSIC]: 'Classic',
-    [GameMode.ECLIPSE]: 'Eclipse',
-    [GameMode.SIMULACRUM]: 'Simulacrum'
+  [GameMode.CLASSIC]: "Classic",
+  [GameMode.ECLIPSE]: "Eclipse",
+  [GameMode.SIMULACRUM]: "Simulacrum",
+};
+
+interface ServerProps {
+  id: string;
+  name: string;
+  gameMode: GameMode;
+}
+
+function Server({ id, name, gameMode }: ServerProps) {
+  const [popoverShow, setPopoverShow] = useState(false);
+  const btnRef = createRef<HTMLDivElement>();
+  const popoverRef = createRef<HTMLDivElement>();
+
+  const openPopover = () => {
+    invariant(btnRef.current, "Button ref should be set");
+    invariant(popoverRef.current, "Popover ref should be set");
+    createPopper(btnRef.current, popoverRef.current, {
+      placement: "left",
+    });
+    setPopoverShow(true);
+  };
+
+  const closePopover = () => {
+    setPopoverShow(false);
+  };
+
+  const onClick = () => {
+    popoverShow ? closePopover() : openPopover();
+  };
+
+  return (
+    <div
+      key={id}
+      className="flex border-b border-solid border-gray-200 px-3 py-3"
+    >
+      <Link className="" to={`/servers/${id}`}>
+        {name}
+      </Link>
+      <div className="ml-3 max-h-max self-center text-xs italic text-gray-500">
+        {GAME_MODE_TRANSLATIONS[gameMode]}
+      </div>
+      <div
+        className="float-right ml-auto cursor-pointer self-center"
+        ref={btnRef}
+        onClick={onClick}
+      >
+        <FaEllipsisV />
+      </div>
+      <div
+        ref={popoverRef}
+        className={
+          (popoverShow ? "" : "hidden ") +
+          "rounded-md border border-solid border-gray-300 bg-white px-0"
+        }
+      >
+        <Link to={`/servers/${id}/delete`}>
+          <div className="cursor-pointer px-3 py-1 text-red-500 hover:bg-gray-200">
+            <FaTrash className="inline" /> Delete
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default function Servers() {
-    const servers = useLoaderData<typeof loader>()
+  const servers = useLoaderData<typeof loader>();
+  invariant(servers, "Servers should be defined");
+
+  const serverComponents = servers.map((server) => {
+    invariant(server.activeProfile, "Active profile should be set");
+    return (
+      <Server
+        key={server.id}
+        id={server.id}
+        name={server.name}
+        gameMode={server.activeProfile.gameMode}
+      />
+    );
+  });
 
   return (
-    <div className="flex min-h-full flex-col justify-center">
-      <div className="mx-auto w-full max-w-lg rounded border border-gray-200 px-8 py-8">
-        <h1 className="mb-4 text-2xl font-bold">Servers</h1>
-        <p>
-            Below are a list of servers currently configured
-        </p>
-
-        <table className="border-collapse border rounded border-slate-500 w-full mt-3">
-            <thead className="bg-slate-300">
-                <tr>
-                    <th>Name</th>
-                    <th>Game Mode</th>
-                    <th>Active Profile</th>
-                </tr>
-            </thead>
-            <tbody>
-                {servers.map(server => {
-                    invariant(server.activeProfile, `Server ${server.id} must have an active profile`)
-                    return (
-                        <tr key={server.id} className="text-center">
-                            <td>{server.name}</td>
-                            <td>{GAME_MODE_TRANSLATIONS[server.activeProfile.gameMode]}</td>
-                            <td>{server.activeProfile.name}</td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
-        <Link to="/servers/new" className="block mt-4 text-center bg-blue-500 hover:bg-blue-600 px-4 py-2 text-white rounded">Create</Link>
+    <div className="min-h-full flex-col">
+      <div className="mx-auto mt-3 w-full max-w-[50%]">
+        <h1 className="pb-3 text-center text-5xl">Servers</h1>
+        <div className="mt-3 mb-3">{serverComponents}</div>
+        <Link
+          to={"create"}
+          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 inline-block"
+        >
+          Create
+        </Link>
       </div>
     </div>
   );
