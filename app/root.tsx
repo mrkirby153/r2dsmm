229541c4763cea8b1bb6bd5ad3723277526e0830
mrkirby153 <mr.austinwhyte@gmail.com>
2022-12-31 15:58:23 -0800

    Add global error boundary

--- app/root.tsx
+++ app/root.tsx
@@ -11,6 +11,7 @@ import {
 
 import tailwindStylesheetUrl from "./styles/tailwind.css";
 import { getUser } from "./session.server";
+import { useState } from "react";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
@@ -26,6 +27,46 @@ export async function loader({ request }: LoaderArgs) {
   return json({
     user: await getUser(request),
   });
+}
+
+const DisplayErrorToggle = ({ error }: { error: Error }) => {
+  const [displayError, setDisplayError] = useState(false);
+  if (process.env.NODE_ENV !== "development") {
+    return null;
+  }
+
+  if (displayError) {
+    return <pre className="mt-2 rounded-md bg-gray-100 p-4">{error.stack}</pre>;
+  } else {
+    return (
+      <button
+        onClick={() => setDisplayError(true)}
+        className="mt-2 rounded bg-red-500 py-2 px-4 font-bold text-white hover:bg-red-700"
+      >
+        Stacktrace
+      </button>
+    );
+  }
+};
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body className="m-4">
+        <h1 className="text-3xl">An Error Occurred</h1>
+        <p className="mt-2 rounded border border-red-300 bg-red-100 p-4">
+          {error.message}
+        </p>
+        <DisplayErrorToggle error={error} />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
 }
 
 export default function App() {
